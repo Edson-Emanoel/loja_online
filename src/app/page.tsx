690d35869af10b74423752f4bdf515d5a973580c
Edson-Emanoel <edson.emanoelofc@gmail.com>
2024-11-01 18:06:39 -0300

    Fiz a listagem estática

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,167 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="containe">
+      <header className="header flex items-center justify-center">
+        <header className="header-item flex items-center justify-between px-5 gap-5">
+          <img src="/logo.png" width={140} height={60} alt="Logo da Loja" className="logoImg" />
+          <ul className="links flex gap-4">
+            <li>
+              <a href="#" className="text-xl font-bold"> Novo Produto </a>
+            </li>
+            <li>
+              <a href="#" className="text-xl font-bold"> Nova Categoria </a>
+            </li>
+            <li>
+              <a href="#" className="text-xl font-bold"> Nova Marca </a>
+            </li>
+          </ul>
+          <img src="/bag.png" alt="" />
+        </header>
+      </header>
+      
+      <main className="cards mt-16 flex flex-wrap gap-5 items-center justify-center mx-auto">
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
+
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
+
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
+
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
+
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
+
+          <div className="card flex flex-col items-center justify-center">
+            <img src="/jaqueta/jaqueta01.png" width={250} height={350} alt="" />
+            
+            <h3 className="text-center text-xl font-bold">Jaqueta Especial</h3>
+            
+            <div className="others flex gap-4">
+              <div className="stars flex">
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+                <img src="/star.svg" width={23} height={22} alt="estrela" /> 
+              </div>
+              
+              <span className="text-lg text-medium">(9)</span>
+              
+              <span className="text-lg text-medium">R$799,99</span>
+            </div>
+          </div>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
